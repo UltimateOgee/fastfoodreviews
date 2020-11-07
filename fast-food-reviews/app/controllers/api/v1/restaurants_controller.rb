0581bdf0ca1a4cff8b7657d472683404cc9bc5c0
BuildTools <unconfigured@null.spigotmc.org>
2020-11-07 09:59:04 -0800

    finished controllers (untested)

--- fast-food-reviews/app/controllers/api/v1/restaurants_controller.rb
+++ fast-food-reviews/app/controllers/api/v1/restaurants_controller.rb
@@ -4,22 +4,54 @@ module Api
             #get all restaurants from db and render them using serializer
             def index
                 restaurants = Restaurant.all
-                render json: RestaurantSerializer.new(restaurants).serialized_json
+                render json: RestaurantSerializer.new(restaurants, options).serialized_json
             end
 
-            #get specific restaurants based on slug from db and render them using serializer
+            #get specific restaurant based on slug from db and render it using serializer
             def show
                 restaurant = Restaurant.find_by(slug: params[:slug])
-                render json: RestaurantSerializer.new(restaurant).serialized_json
+                render json: RestaurantSerializer.new(restaurant, options).serialized_json
             end
 
             def create
+                restaurant = Restaurant.new(restaurant_params)
+
+                if restaurant.save
+                    render json: RestaurantSerializer.new(restaurant).serialized_json
+                else
+                    render json: { error: restaurant.errors.messages }, status: 422
+                end
+            end
+
+            def update
+                restaurant = Restaurant.find_by(slug: params[:slug])
+
+                if restaurant.update(restaurant_params)
+                    render json: RestaurantSerializer.new(restaurant, options).serialized_json
+                else
+                    render json: { error: restaurant.errors.messages }, status: 422
+                end
+            end
+
+            def destroy
+                restaurant = Restaurant.find_by(slug: params[:slug])
+
+                if restaurant.destroy
+                    head :no_content
+                else
+                    render json: { error: restaurant.errors.messages }, status: 422
+                end
             end
 
             private
 
             def restaurant_params
-                
+                params.require(:restaurant).permit(:name ,:image_url)
+            end
+
+            def options
+                @options ||= { include: %i[reviews] }
+            end
 
         end
     end
